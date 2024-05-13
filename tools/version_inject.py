a33ea80b76af4a82092e1fea5d637dc611633975
Whth <88489697+Whth@users.noreply.github.com>
2024-05-13 17:32:21 +0800

    ci(version): add version tool

---
+++ tools/version_inject.py
@@ -0,0 +1,18 @@
+from toml import load
+
+
+with open("pyproject.toml", "r") as fp:
+    pyproject_toml = load(fp)
+
+
+package_version = pyproject_toml.get("project").get("version")
+
+with open("src/kazu/__init__.py", "r") as fp:
+    __init__ = fp.read()
+
+tokens = __init__.split("=")
+tokens.pop()
+tokens.append(f' "{package_version}"\n')
+
+with open("src/kazu/__init__.py", "w") as fp:
+    fp.write("=".join(tokens))
