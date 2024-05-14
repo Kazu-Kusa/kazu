ae378410d57f4dafd18cb161ee11472e1d692196
Whth <88489697+Whth@users.noreply.github.com>
2024-05-14 17:55:20 +0800

    feat(cli): build device checker into check cmd

--- src/kazu/cli.py
+++ src/kazu/cli.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from time import sleep
 from typing import Callable, TextIO, Optional, Tuple
 
 import bdmc
@@ -22,7 +23,7 @@ def _set_all_log_level(level: int | str):
 
 
 @click.group(
-    epilog="For more details, Check at https://github.com/Kazu-Kusa/kazu",
+    epilog=r"For more details, Check at https://github.com/Kazu-Kusa/kazu",
 )
 @click.help_option("-h", "--help")
 @click.version_option(__version__, "-v", "--version", prog_name=__command__)
@@ -105,12 +106,84 @@ def run(use_camera: bool, team_color: str, run_config: TextIO | None, mode: str)
 
 @main.command("check")
 @click.help_option("-h", "--help")
-@click.argument("device", type=click.Choice(["mot", "cam", "led", "lcd", "adc", "io", "mpu", "pow", "all"]))
-def test(device: str):
+@click.pass_context
+@click.argument("device", type=click.Choice(devs := ["mot", "cam", "adc", "io", "mpu", "pow", "all"]), nargs=-1)
+def test(ctx: click.Context, device: str):
     """
-    Check devices.
+    Check devices' normal functions
     """
-    print(device)
+    app_config: APPConfig = ctx.obj.app_config
+
+    from .checkers import check_io, check_camera, check_adc, check_motor, check_power, check_mpu
+    from terminaltables import SingleTable
+    from colorama import Fore
+
+    shader = lambda dev_name, success: [
+        f"{Fore.LIGHTYELLOW_EX if success else Fore.RED}{dev_name}{Fore.RESET}",
+        f"{Fore.GREEN if success else Fore.RED}{success}{Fore.RESET}",
+    ]
+
+    table = [[f"{Fore.YELLOW}Device{Fore.RESET}", f"{Fore.GREEN}Success{Fore.RESET}"]]
+    if "all" in device or device == ():
+        from bdmc import CMD
+        from cv2 import VideoCapture
+        from .compile import controller, sensors
+
+        controller.serial_client.port = app_config.motion.port
+
+        controller.serial_client.open()
+        controller.start_msg_sending().send_cmd(CMD.RESET)
+        table.append(shader("IO", check_io(sensors)))
+        table.append(shader("ADC", check_adc(sensors)))
+        table.append(shader("MPU", check_mpu(sensors)))
+        table.append(shader("POWER", check_power(sensors)))
+        table.append(shader("MOTOR", check_motor(controller)))
+        table.append(shader("CAMERA", check_camera(VideoCapture(app_config.vision.camera_device_id))))
+        secho(
+            SingleTable(table).table,
+            fg="green",
+        )
+        controller.stop_msg_sending()
+        return
+
+    if "adc" in device:
+        from .compile import sensors
+
+        table.append(shader("ADC", check_adc(sensors)))
+
+    if "io" in device:
+        from .compile import sensors
+
+        table.append(shader("IO", check_io(sensors)))
+
+    if "mpu" in device:
+        from .compile import sensors
+
+        table.append(shader("MPU", check_mpu(sensors)))
+
+    if "pow" in device:
+        from .compile import sensors
+
+        table.append(shader("POWER", check_power(sensors)))
+
+    if "cam" in device:
+        from cv2 import VideoCapture
+
+        table.append(shader("CAMERA", check_camera(VideoCapture(app_config.vision.camera_device_id))))
+
+    if "mot" in device:
+        from .compile import controller, sensors
+        from bdmc import CMD
+
+        controller.serial_client.port = app_config.motion.port
+
+        controller.serial_client.open()
+        controller.start_msg_sending().send_cmd(CMD.RESET)
+        table.append(shader("MOTOR", check_motor(controller)))
+
+    secho(
+        SingleTable(table).table,
+    )
 
 
 @main.command("cmd")
@@ -131,17 +204,41 @@ def control_motor(ctx: click.Context, duration: float, speeds: list[int]):
         DURATION: (float)
     """
     from .compile import composer, botix, controller
+    from colorama import Fore
 
     internal_conf: _InternalConfig = ctx.obj
     controller.serial_client.port = internal_conf.app_config.motion.port
-    states, transitions = (
-        composer.init_container()
-        .add(MovingState(*speeds))
-        .add(MovingTransition(duration))
-        .add(MovingState(0))
-        .export_structure()
-    )
+    controller.serial_client.open()
+    controller.start_msg_sending()
+    try:
+        states, transitions = (
+            composer.init_container()
+            .add(state := MovingState(*speeds))
+            .add(MovingTransition(duration))
+            .add(MovingState(0))
+            .export_structure()
+        )
+    except ValueError as e:
+        secho(f"{e}", fg="red")
+        return
+
     botix.token_pool = transitions
 
+    secho(
+        f"Move as {Fore.YELLOW}{state.unwrap()}{Fore.RESET} for {Fore.YELLOW}{duration}{Fore.RESET} seconds",
+    )
     fi: Callable[[], None] = botix.compile(return_median=False)
+
+    def _bar():
+        with click.progressbar(
+            range(int(duration / 0.1)), show_percent=True, show_eta=True, label="Moving", color=True
+        ) as bar:
+            for _ in bar:
+                sleep(0.1)
+
+    import threading
+
+    t = threading.Thread(target=_bar, daemon=True)
+    t.start()
     fi()
+    controller.stop_msg_sending()
