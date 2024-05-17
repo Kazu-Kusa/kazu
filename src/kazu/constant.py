df84e436a276c11a0d236eb397dc8612ce00ab65
Whth <88489697+Whth@users.noreply.github.com>
2024-05-17 16:52:28 +0800

    refactor(constant): better constant

--- src/kazu/constant.py
+++ src/kazu/constant.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import List
+from typing import List, Tuple
 
 
 @dataclass(frozen=True)
@@ -40,6 +40,50 @@ class RunMode:
         return [RunMode.AFG, RunMode.ANG, RunMode.NGS, RunMode.FGS, RunMode.FGDL]
 
 
+@dataclass(frozen=True)
+class EdgeWeights:
+
+    FL: int = 1
+    FR: int = 2
+    RL: int = 4
+    RR: int = 8
+
+    @staticmethod
+    def export_std_weight_seq() -> Tuple[int, int, int, int]:
+        return EdgeWeights.FL, EdgeWeights.RL, EdgeWeights.RR, EdgeWeights.FR
+
+
+@dataclass(frozen=True)
+class FenceWeights:
+    Front: int = 1
+    Rear: int = 2
+
+    Left: int = 4
+    Right: int = 8
+
+
+@dataclass(frozen=True)
+class SurroundingWeights:
+    # region SURROUNDING KEYS
+    LEFT_OBJECT = 1
+    RIGHT_OBJECT = 2
+    BEHIND_OBJECT = 4
+    LEFT_RIGHT_OBJECTS = 3
+    LEFT_BEHIND_OBJECTS = 5
+    RIGHT_BEHIND_OBJECTS = 6
+    LEFT_RIGHT_BEHIND_OBJECTS = 7
+    # endregion
+
+    # region BASIC KEYS
+    FRONT_ENEMY_CAR = 400
+    FRONT_ENEMY_BOX = 300
+    FRONT_NEUTRAL_BOX = 200
+    FRONT_ALLY_BOX = 100
+    NOTHING = 0
+
+    # endregion
+
+
 class EdgeCodeSign(Enum):
     """
     fl           fr
@@ -54,25 +98,25 @@ class EdgeCodeSign(Enum):
 
     O_O_O_O = 0
 
-    X_O_O_O = 1  # Front-Left encounter edge
-    O_O_O_X = 2  # Front-Right encounter edge
-    O_X_O_O = 4  # Rear-Left encounter edge
-    O_O_X_O = 8  # Rear-Right encounter edge
+    X_O_O_O = EdgeWeights.FL  # Front-Left encounter edge
+    O_O_O_X = EdgeWeights.FR  # Front-Right encounter edge
+    O_X_O_O = EdgeWeights.RL  # Rear-Left encounter edge
+    O_O_X_O = EdgeWeights.RR  # Rear-Right encounter edge
 
     # A bunches of combinations
-    X_X_O_O = 5
-    X_O_X_O = 9
-    X_O_O_X = 3
-    O_X_X_O = 12
-    O_X_O_X = 6
-    O_O_X_X = 10
+    X_X_O_O = EdgeWeights.FL + EdgeWeights.RL
+    O_O_X_X = EdgeWeights.RR + EdgeWeights.FR
+    X_O_X_O = EdgeWeights.FL + EdgeWeights.RR
+    X_O_O_X = EdgeWeights.FR + EdgeWeights.FL
+    O_X_X_O = EdgeWeights.RL + EdgeWeights.RR
+    O_X_O_X = EdgeWeights.RL + EdgeWeights.FR
 
-    X_X_X_O = 13
-    X_X_O_X = 7
-    X_O_X_X = 11
-    O_X_X_X = 14
+    X_X_X_O = EdgeWeights.FL + EdgeWeights.RL + EdgeWeights.RR
+    X_X_O_X = EdgeWeights.FL + EdgeWeights.FR + EdgeWeights.RL
+    X_O_X_X = EdgeWeights.FL + EdgeWeights.RR + EdgeWeights.FR
+    O_X_X_X = EdgeWeights.RL + EdgeWeights.RR + EdgeWeights.FR
 
-    X_X_X_X = 15
+    X_X_X_X = EdgeWeights.FL + EdgeWeights.RL + EdgeWeights.RR + EdgeWeights.FR
 
 
 class SurroundingCodeSign(Enum):
@@ -88,29 +132,19 @@ class SurroundingCodeSign(Enum):
 
     # region SURROUNDING KEYS
     LEFT_OBJECT = 1
-
     RIGHT_OBJECT = 2
-
     BEHIND_OBJECT = 4
-
     LEFT_RIGHT_OBJECTS = 3
-
     LEFT_BEHIND_OBJECTS = 5
-
     RIGHT_BEHIND_OBJECTS = 6
-
     LEFT_RIGHT_BEHIND_OBJECTS = 7
     # endregion
 
     # region BASIC KEYS
     FRONT_ENEMY_CAR = 400
-
     FRONT_ENEMY_BOX = 300
-
     FRONT_NEUTRAL_BOX = 200
-
     FRONT_ALLY_BOX = 100
-
     NOTHING = 0
 
     # endregion
